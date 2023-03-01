636e1ad9bf07262d9fd67a44d936f6fe02c088cc
Khomsan Chooon <85799327+MarkKh@users.noreply.github.com>
2023-03-01 21:26:22 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'แอพบัญชี'),
+        home: const MyHomePage(title: 'Movie Management'),
       ),
     );
   }
